f918daecbb5450949c55a83fd0de9eec3a0dec75
carlos pizano <carlos.pizano@gmail.com>
2025-02-26 21:35:42 -0800

    Add the benchmark
    With all the preliminaries done, we finally get to bench the SSM.
    
    Now comes the fun part, get stable numbers.... somehow.

--- src/main.zig
+++ src/main.zig
@@ -19,7 +19,7 @@ pub fn main() !void {
 
     try stdout.print("benchmarking zig std library:\n", .{});
 
-    try staticStringMapBench(allocator);
+    try staticStringMapBench(allocator, &stdout);
 }
 
 pub fn readFile(allocator: Allocator, filename: []const u8) ![]u8 {
@@ -29,9 +29,8 @@ pub fn readFile(allocator: Allocator, filename: []const u8) ![]u8 {
     return try file.readToEndAlloc(allocator, stat.size);
 }
 
-pub fn staticStringMapBench(allocator: Allocator) !void {
-    const data = try readFile(allocator, "input/cat_lib_00.txt");
-    defer allocator.free(data);
+pub fn generateTokens(allocator: Allocator, data: []const u8) !std.ArrayList([]const u8) {
+    var final_tokens = std.ArrayList([]const u8).init(allocator);
 
     const delimiters = [_]u8{ //.
         ' ', '\n', '.', ',', ';', '[', ']', '(', ')', '{', '}', //.
@@ -47,7 +46,6 @@ pub fn staticStringMapBench(allocator: Allocator) !void {
     var state: State = .Code;
     var strings: u32 = 0;
     var comments: u32 = 0;
-    var code: u32 = 0;
 
     var tokens = std.mem.splitAny(u8, data, &delimiters);
     while (tokens.next()) |token| {
@@ -63,8 +61,7 @@ pub fn staticStringMapBench(allocator: Allocator) !void {
                     state = .Comment;
                     continue :sw .Comment;
                 } else {
-                    code += 1;
-                    std.debug.print("{s}\n", .{token});
+                    try final_tokens.append(token);
                 }
             },
             .Comment => {
@@ -81,8 +78,160 @@ pub fn staticStringMapBench(allocator: Allocator) !void {
             },
         }
     }
-    std.log.info("code: {}, comments: {}, strings {}", .{ code, comments, strings });
+    std.log.info("code: {}, comments: {}, strings {}", .{ final_tokens.items.len, comments, strings });
+    return final_tokens;
+}
+
+pub fn staticStringMapBench(allocator: Allocator, writer: anytype) !void {
+    const data = try readFile(allocator, "input/cat_lib_00.txt");
+    defer allocator.free(data);
+
+    var tokens = try generateTokens(allocator, data);
+    defer tokens.deinit();
+
+    const Tag = enum {
+        keyword_addrspace,
+        keyword_align,
+        keyword_allowzero,
+        keyword_and,
+        keyword_anyframe,
+        keyword_anytype,
+        keyword_asm,
+        keyword_async,
+        keyword_await,
+        keyword_break,
+        keyword_callconv,
+        keyword_catch,
+        keyword_comptime,
+        keyword_const,
+        keyword_continue,
+        keyword_defer,
+        keyword_else,
+        keyword_enum,
+        keyword_errdefer,
+        keyword_error,
+        keyword_export,
+        keyword_extern,
+        keyword_fn,
+        keyword_for,
+        keyword_if,
+        keyword_inline,
+        keyword_noalias,
+        keyword_noinline,
+        keyword_nosuspend,
+        keyword_opaque,
+        keyword_or,
+        keyword_orelse,
+        keyword_packed,
+        keyword_pub,
+        keyword_resume,
+        keyword_return,
+        keyword_linksection,
+        keyword_struct,
+        keyword_suspend,
+        keyword_switch,
+        keyword_test,
+        keyword_threadlocal,
+        keyword_try,
+        keyword_union,
+        keyword_unreachable,
+        keyword_usingnamespace,
+        keyword_var,
+        keyword_volatile,
+        keyword_while,
+    };
+
+    const keywords = std.StaticStringMap(Tag).initComptime(.{
+        .{ "addrspace", .keyword_addrspace },
+        .{ "align", .keyword_align },
+        .{ "allowzero", .keyword_allowzero },
+        .{ "and", .keyword_and },
+        .{ "anyframe", .keyword_anyframe },
+        .{ "anytype", .keyword_anytype },
+        .{ "asm", .keyword_asm },
+        .{ "async", .keyword_async },
+        .{ "await", .keyword_await },
+        .{ "break", .keyword_break },
+        .{ "callconv", .keyword_callconv },
+        .{ "catch", .keyword_catch },
+        .{ "comptime", .keyword_comptime },
+        .{ "const", .keyword_const },
+        .{ "continue", .keyword_continue },
+        .{ "defer", .keyword_defer },
+        .{ "else", .keyword_else },
+        .{ "enum", .keyword_enum },
+        .{ "errdefer", .keyword_errdefer },
+        .{ "error", .keyword_error },
+        .{ "export", .keyword_export },
+        .{ "extern", .keyword_extern },
+        .{ "fn", .keyword_fn },
+        .{ "for", .keyword_for },
+        .{ "if", .keyword_if },
+        .{ "inline", .keyword_inline },
+        .{ "noalias", .keyword_noalias },
+        .{ "noinline", .keyword_noinline },
+        .{ "nosuspend", .keyword_nosuspend },
+        .{ "opaque", .keyword_opaque },
+        .{ "or", .keyword_or },
+        .{ "orelse", .keyword_orelse },
+        .{ "packed", .keyword_packed },
+        .{ "pub", .keyword_pub },
+        .{ "resume", .keyword_resume },
+        .{ "return", .keyword_return },
+        .{ "linksection", .keyword_linksection },
+        .{ "struct", .keyword_struct },
+        .{ "suspend", .keyword_suspend },
+        .{ "switch", .keyword_switch },
+        .{ "test", .keyword_test },
+        .{ "threadlocal", .keyword_threadlocal },
+        .{ "try", .keyword_try },
+        .{ "union", .keyword_union },
+        .{ "unreachable", .keyword_unreachable },
+        .{ "usingnamespace", .keyword_usingnamespace },
+        .{ "var", .keyword_var },
+        .{ "volatile", .keyword_volatile },
+        .{ "while", .keyword_while },
+    });
+
+    // The keywords strings is about 850 bytes if densely packed, but the SSM
+    // stores them as  `keys: [*]const []const u8` so it is unclear how this data
+    // is stored at comptime, or for that matter, at runtime.
+
+    var hits: u32 = 0;
+    var misses: u32 = 0;
+
+    var timer = std.time.Timer.start() catch @panic("need timer to work");
+
+    const start_time = timer.read();
+
+    for (tokens.items) |token| {
+        if (keywords.get(token) != null) {
+            hits += 1;
+        } else {
+            misses += 1;
+        }
+    }
+
+    const end_time = timer.read();
+
+    const elapsed_time_us = (end_time - start_time) / 1000;
+    const per_item_ns = (end_time - start_time) / tokens.items.len;
+
+    std.log.info("keyword:  hits {}, misses", .{ hits, misses });
+
+    try writer.print("staticStringMap: wall = {} uS,  per item = {} nS, miss/hit ratio = {} \n", //.
+        .{ elapsed_time_us, per_item_ns, misses / hits });
 }
 
 const std = @import("std");
 const lib = @import("zbench_lib");
+
+//  So far:
+//  zig build run -Doptimize=ReleaseFast
+//
+// benchmarking zig std library:
+// staticStringMap: wall = 3923 uS,  per item = 17 nS, miss/hit = 3
+//
+// benchmarking zig std library:
+// staticStringMap: wall = 4340 uS,  per item = 19 nS, miss/hit = 3
+//
