81237b14d1862606dd4ed8b8c8adee2b14287cbc
carlos pizano <carlos.pizano@gmail.com>
2025-02-26 19:17:20 -0800

    Decent tokenizer
    Not going to win awards but the tokenizer
    skips most of the noise, so what it is left
    is pretty much code which should be either
    identifiers or keywords.
    
    The current verison prints the first 200
    so you can manually check vs the top of the
    cat_lib_00.txt file.

--- src/main.zig
+++ src/main.zig
@@ -33,14 +33,63 @@ pub fn staticStringMapBench(allocator: Allocator) !void {
     const data = try readFile(allocator, "input/cat_lib_00.txt");
     defer allocator.free(data);
 
-    const delimiters = [_]u8{ ' ', '\n', '.', ',', '"', ';', '[', ']', '(', ')', '{', '}', ':', '!', '=', '&', '*', '|' };
+    const delimiters = [_]u8{ //.
+        ' ', '\n', '.', ',', ';', '[', ']', '(', ')', '{', '}', //.
+        ':', '!',  '=', '&', '*', '|', '?', '+', '-', '>', '<',
+    };
+
+    const State = enum {
+        Code,
+        Comment,
+        String,
+    };
+
+    var state: State = .Code;
+    var strings: u32 = 0;
+    var comments: u32 = 0;
+    var code: u32 = 0;
+
+    var stop: u32 = 200;
 
     var tokens = std.mem.splitAny(u8, data, &delimiters);
     while (tokens.next()) |token| {
-        if (!std.mem.eql(u8, token, "")) {
-            std.debug.print("{s}\n", .{token});
+        if (token.len == 0)
+            continue;
+
+        if (stop > 0) {
+            stop -= 1;
+        } else {
+            break;
+        }
+
+        sw: switch (state) {
+            .Code => {
+                if (token[0] == '"') {
+                    state = .String;
+                    continue :sw .String;
+                } else if (std.mem.startsWith(u8, token, "//")) {
+                    state = .Comment;
+                    continue :sw .Comment;
+                } else {
+                    code += 1;
+                    std.debug.print("{s}\n", .{token});
+                }
+            },
+            .Comment => {
+                if (tokens.rest().len != 0 and tokens.rest()[0] == '\n') {
+                    state = .Code;
+                    comments += 1;
+                }
+            },
+            .String => {
+                if (token[token.len - 1] == '"') {
+                    state = .Code;
+                    strings += 1;
+                }
+            },
         }
     }
+    std.log.info("code: {}, comments: {}, strings {}", .{ code, comments, strings });
 }
 
 const std = @import("std");
