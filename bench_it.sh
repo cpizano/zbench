2bc392d13d61f6f1d89db3b2ffcc48b1b7c71389
carlos pizano <carlos.pizano@gmail.com>
2025-03-08 13:41:54 -0800

    Improve the shell script
    Now it kind-of-works.

--- bench_it.sh
+++ bench_it.sh
@@ -1,13 +1,32 @@
-# bash
+#!/bin/bash
 
+# Script to build and benchmark with both the old and new version of the
+# standard library.
+#
 # Must be run from the zbench root dir.
+set -e
 
-# Build both versions.
-zig build --release=fast  --zig-lib-dir /Users/cpu/src/zig/lib --prefix zig-out/new
-zig build --release=fast --prefix zig-out/old
+path_old=zig-out/old
+path_new=zig-out/new
+bin_old=$path_old/bin/zbench
+bin_new=$path_new/bin/zbench
 
-# Run each.
+# Build both versions. if not there.
+if [ ! -f "$bin_new" ]; then
+echo "build new version."
+zig build --release=fast  --zig-lib-dir /Users/cpu/src/zig/lib --prefix "$path_new"
+fi
+
+if [ ! -f "$bin_old" ]; then
+echo "build old version."
+zig build --release=fast --prefix "$path_old"
+fi
+
+# Run both versions using the "background" policy for MacOS which should
+# restrict to using the efficiency cores.
 echo "new:"
-zig-out/new/bin/zbench
+taskpolicy -b "$bin_new"
 echo "old:"
-zig-out/old/bin/zbench
+taskpolicy -b "$bin_old"
+
+echo "done."
